fb004c367244871275e5185df2a686c8567f9932
hotdocx <hotdocx@users.noreply.github.com>
2026-02-01 18:55:18 +0530

    deploy: hotdocx spa

---
+++ assets/gallery-D5xmSvcL.js
@@ -0,0 +1,1 @@
+import{j as r,E as t}from"./main-BLVQhiyR.js";const p=({error:o})=>r.jsx(t,{error:o});export{p as errorComponent};
--- assets/gallery-yIv0jAa1.js
+++
@@ -1,1 +0,0 @@
-import{j as r,E as t}from"./main-9wsZgjfg.js";const p=({error:o})=>r.jsx(t,{error:o});export{p as errorComponent};
